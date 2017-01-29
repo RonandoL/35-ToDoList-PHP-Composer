b5d5c3d9723dc28fc67fae62a8b432a58c542f89
Ronando Long <ronando.long@gmail.com>
2017-01-28 16:51:15 -0800

    add constructor with one paramater

---
+++ src/tasks.php
@@ -0,0 +1,15 @@
+<?php
+    class Tasks
+    {
+        private $description;
+
+        function __construct($task_description)
+        {
+            $task->description = $task_description;
+        }
+
+        //SETTER and GETTER
+        
+
+    }
+?>
